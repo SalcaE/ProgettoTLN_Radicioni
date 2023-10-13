fa31d1e7466ff5dde6d726c1726221080d91462e
SpanuAlessandro <SpanuAlessandro@users.noreply.github.com>
2023-10-13 11:42:32 +0200

    super ricorsione

--- Esercizio2/elaborazione.py
+++ Esercizio2/elaborazione.py
@@ -2,6 +2,7 @@ import pandas as pd
 import spacy
 from nltk.corpus import framenet as fn
 from nltk.corpus import wordnet as wn
+import numpy as np
 
 '''def frame_elaboration(row):
     print("print frame",row)
@@ -85,23 +86,68 @@ def elaboration_def(definition):
     return set(tokens)
 
 def calculate_score(syn,gold,score):
-    
-    if gold[1] and syn and str(syn) == gold[1]:
+    syn_gold = gold.values
+    if syn_gold[0] and syn and str(syn) == str(syn_gold[0]):
         score = score+1
         return score
    
     return score
+
+def get_paths_between_synsets(syn_w, syn_el, L=3):
+    paths = []
+    visited = set()
+
+    def dfs(synset, path):
+        if synset in visited or len(path) > L:
+            return
+        if synset == syn_el:
+            paths.append(path + [synset])
+            return
+        visited.add(synset)
+        for hypernym in synset.hypernyms():
+            dfs(hypernym, path + [synset])
+        for hyponym in synset.hyponyms():
+            dfs(hyponym, path + [synset])
+
+    dfs(syn_w, [])
+    return [path for path in paths if len(path) <= L]
+
+def score(syn_el, ctx_el):
+    res = 0
+    for word in ctx_el:   #prendo token definizione
+        for syn_w in wn.synsets(word):
+            #get all connection path between syn and syn_fn
+            paths = list(get_paths_between_synsets(syn_w, syn_el, 3))
+            print(paths)
+            for path in paths:
+                res += np.exp(-len(path)-1)
+
+    return res
+
+
+def prob(syn_el, element, ctx_el):
+    sum = 0
+
+    for syn in wn.synsets(element): #riscorre..
+        sum += score(syn,ctx_el)
+
+    return score(syn_el, ctx_el) / sum
   
+def argmax_prob(element, ctx_el):
+    max_prob = 0
+    max_syn = None
+    for syn in wn.synsets(element):  #synset sull'elment/frame principale
+        prob_syn = prob(syn, element, ctx_el)
+        if prob_syn > max_prob:
+            max_prob = prob_syn
+            max_syn = syn
+            
+    return max_syn
 
 def getData():
     data = pd.read_csv("ProgettoTLN_Radicioni\\Esercizio2\\finale2.csv")
     #print(data)
     
-
-
-
-    ciaone=data[data['ID']== 8827]
-
     frames=[1604,1916,2284,221,2131,2046,269,2940,2430,1919]
     
     #data.to_csv('ProgettoTLN_Radicioni\Esercizio2\csvProva.csv')
@@ -115,23 +161,26 @@ def getData():
         #loll= eval(typeRow+'_elaboration' + "(row)")
     score = 0
     tot = 0
+    
     for frame_id in frames:
         frame= fn.frame_by_id(frame_id)
         
         context_frame = ctx_frame(frame)
         tot = tot+1
-        res_syn_frame= bag_of_words(frame.name,context_frame)
+       
+        #res_syn_frame= bag_of_words(frame.name,context_frame)
 
-        syn_gold_frame=data[data['ID']== frame.ID]['Syn']
-        score = calculate_score(res_syn_frame,syn_gold_frame,score)
+        #syn_gold_frame=data[data['ID']== frame.ID]['Syn']
+        #score = calculate_score(res_syn_frame,syn_gold_frame,score)
+
+        argmax_prob(frame.name, context_frame)
         
-        for key,el in frame.FE.items():
+        '''for key,el in frame.FE.items():
             context_fe = ctx_fe(el)
             tot = tot+1
             res_syn_fe= bag_of_words(el.name,context_fe)
 
             syn_gold=data[data['ID']== el.ID]['Syn']
-            
             
             score = calculate_score(res_syn_fe,syn_gold,score)
         
@@ -144,19 +193,12 @@ def getData():
 
             syn_gold=data[data['ID']== el.ID]['Syn']
             
-            score = calculate_score(res_syn_fe,syn_gold,score)
+            score = calculate_score(res_syn_fe,syn_gold,score)'''
             
       
-        print(score/tot)
+    print(score/tot)
         
        
-   
-        
-        
-
-    
-    
-    ciao = 1
 
     
     
