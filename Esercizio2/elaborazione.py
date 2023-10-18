01f067b664c7b516ea7af2ff3f1e6205595a2aab
SpanuAlessandro <SpanuAlessandro@users.noreply.github.com>
2023-10-18 12:04:37 +0200

    es2 finito <3

--- Esercizio2/elaborazione.py
+++ Esercizio2/elaborazione.py
@@ -3,30 +3,22 @@ import spacy
 from nltk.corpus import framenet as fn
 from nltk.corpus import wordnet as wn
 import numpy as np
-
-'''def frame_elaboration(row):
-    print("print frame",row)
-
-def fe_elaboration(row):
-    print("print fe")
-
-def lu_elaboration(row):
-    print("print lu")'''
+import re
 
 def ctx_frame(frame):
     definition = frame.definition
-     
     sentence= ''
     for key, fe in frame.FE.items():
         sentence += ' ' + fe.definition
+
+    for key, lu in frame.lexUnit.items():
+        sentence += ' ' + lu.definition    
     
-    return elaboration_def(definition+sentence)
-
-
+    return elaboration_def(definition+sentence+frame.name)
 
 def ctx_fe(fe_element):
 
-    definition = fe_element.definition
+    definition = fe_element.definition + fe_element.name
     
     return elaboration_def(definition)
 
@@ -38,8 +30,7 @@ def ctx_lu(lu_element):
     for ex in exemplars:
         sentence += ' ' + ex.annotationSet[0].text
 
-    return elaboration_def(definition+sentence)
-
+    return elaboration_def(definition+sentence+lu_element.name)
 
 def ctx_wn(syn):
 
@@ -48,7 +39,6 @@ def ctx_wn(syn):
     example_def = ''
     for ex in syn.examples():
         example_def+= ' '+ex
-
 
     hypernyms_def= ''
     for hypernym in syn.hypernyms():
@@ -78,138 +68,124 @@ def elaboration_def(definition):
     
     sp = spacy.load('en_core_web_md')
 
+    definition = re.sub(r'[^\w\s]','',definition)
+
     doc = sp(definition)
-   
+
     tokens = [token.lemma_.lower() for token in doc     #tokenizzo le frasi
                         if not token.is_stop and 
                         not token.is_punct and token.text.strip()]
+    
     return set(tokens)
 
 def calculate_score(syn,gold,score):
     syn_gold = gold.values
+    print('grafico: ',str(syn),' gold: ',str(syn_gold[0]))
     if syn_gold[0] and syn and str(syn) == str(syn_gold[0]):
         score = score+1
         return score
    
     return score
 
-def get_paths_between_synsets(syn_w, syn_el, L=3):
-    paths = []
-    visited = set()
+def path_search(synset_w,syn_el, path, paths):
+    tmp = path + [synset_w]  #percorso temporaneo
 
-    def dfs(synset, path):
-        if synset in visited or len(path) > L:
-            return
-        if synset == syn_el:
-            paths.append(path + [synset])
-            return
-        visited.add(synset)
-        for hypernym in synset.hypernyms():
-            dfs(hypernym, path + [synset])
-        for hyponym in synset.hyponyms():
-            dfs(hyponym, path + [synset])
+    if synset_w == syn_el:
+        paths.append(tmp)
+        return paths
+    
+    if  len(tmp) >= 3:
+        return
+    
+    for hypernym in synset_w.hypernyms():
+        path_search(hypernym,syn_el, tmp, paths)
+    for hyponym in synset_w.hyponyms():
+        path_search(hyponym,syn_el, tmp, paths)
+    
+    return paths
 
-    dfs(syn_w, [])
-    return [path for path in paths if len(path) <= L]
-
-def score(syn_el, ctx_el):
+def getScore(syn_el, ctx_el):
     res = 0
     for word in ctx_el:   #prendo token definizione
         for syn_w in wn.synsets(word):
-            #get all connection path between syn and syn_fn
-            paths = list(get_paths_between_synsets(syn_w, syn_el, 3))
-            print(paths)
-            for path in paths:
-                res += np.exp(-len(path)-1)
-                print(paths)
-
+           
+            paths = path_search(syn_w, syn_el,[],[])
+           
+            if paths:   #se trova un percorso
+                for path in paths:
+                    res += np.exp(-len(path)-1)
     return res
-
-
-def prob(syn_el, element, ctx_el):
-    sum = 0
-
-    for syn in wn.synsets(element): #riscorre..
-        sum += score(syn,ctx_el)
-
-    return score(syn_el, ctx_el) / sum
   
 def approccio_grafico(element, ctx_el):
-    max_prob = 0
-    max_syn = None
+    best_syn = None
+    max_score = 0 
+    res = 0
+   
     for syn in wn.synsets(element):  #synset sull'elment/frame principale
-        prob_syn = prob(syn, element, ctx_el)
+        sum = 0
 
+        for syn2 in wn.synsets(element): #riscorre..
+            sum += getScore(syn2,ctx_el)
 
-
-        if prob_syn > max_prob:
-            max_prob = prob_syn
-            max_syn = syn
+        if sum > 0:
+            res = getScore(syn, ctx_el) / sum # score(s,w)/score(s',w')
+   
+        if res > max_score:  #argmax
+            max_score = res
+            best_syn = syn
             
-    return max_syn
+    return best_syn
 
-def getData():
-    data = pd.read_csv("ProgettoTLN_Radicioni\\Esercizio2\\finale2.csv")
-    #print(data)
-    
+def getData(algo_type):
+    data = pd.read_csv("Esercizio2\\annotation.csv")
     frames=[1604,1916,2284,221,2131,2046,269,2940,2430,1919]
     
-    #data.to_csv('ProgettoTLN_Radicioni\Esercizio2\csvProva.csv')
-   # ciaone = pd.read_csv("ProgettoTLN_Radicioni\Esercizio2\csvProva.csv")
-    
-    #for index, row in data.iterrows():
-        #print('indice ',index)
-        #print('row ',row['Type'])
-        #typeRow = row['Type']
-         
-        #loll= eval(typeRow+'_elaboration' + "(row)")
     score = 0
     tot = 0
     
     for frame_id in frames:
-        frame= fn.frame_by_id(frame_id)
-        
-        context_frame = ctx_frame(frame)
         tot = tot+1
+        frame= fn.frame_by_id(frame_id)
+        context_frame = ctx_frame(frame)
+        syn_gold_frame=data[data['ID']== frame.ID]['Syn']
+
+        if algo_type == 'bag_of_words':
+            res_syn_frame= bag_of_words(frame.name,context_frame)
+        else:
+            res_syn_frame=approccio_grafico(frame.name, context_frame)
+
+        score = calculate_score(res_syn_frame,syn_gold_frame,score)
        
-        #res_syn_frame= bag_of_words(frame.name,context_frame)
-
-        #syn_gold_frame=data[data['ID']== frame.ID]['Syn']
-        #score = calculate_score(res_syn_frame,syn_gold_frame,score)
-
-        approccio_grafico(frame.name, context_frame)
-        
-        '''for key,el in frame.FE.items():
-            context_fe = ctx_fe(el)
+        for key,el in frame.FE.items():
             tot = tot+1
-            res_syn_fe= bag_of_words(el.name,context_fe)
-
+            context_fe = ctx_fe(el)
             syn_gold=data[data['ID']== el.ID]['Syn']
             
+            if algo_type == 'bag_of_words':
+                res_syn_fe= bag_of_words(el.name,context_fe)
+            else:
+                res_syn_fe=approccio_grafico(key, context_fe)
+              
             score = calculate_score(res_syn_fe,syn_gold,score)
         
-
-
         for key,el in frame.lexUnit.items():
-            context_lu = ctx_lu(el)
             tot = tot+1
-            res_syn_fe= bag_of_words(el.name,context_lu)
-
+            context_lu = ctx_lu(el)
             syn_gold=data[data['ID']== el.ID]['Syn']
-            
-            score = calculate_score(res_syn_fe,syn_gold,score)'''
-            
-      
-    print(score/tot)
-        
-       
 
-    
-    
-        
+            if algo_type == 'bag_of_words':
+                res_syn_fe= bag_of_words(el.name,context_lu)
+            else:
+                res_syn_fe=approccio_grafico(key, context_lu)
+              
+            score = calculate_score(res_syn_fe,syn_gold,score)
+              
+    return(score/tot)
+
 
 def main():
-    data= getData()
+    score = getData('cazzo ')
+    print(score)
 
 if __name__ == '__main__':
     main()
