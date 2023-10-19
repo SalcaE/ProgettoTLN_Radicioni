0744b3d6c5d98326b67a770fe67d3c11331e2056
Max26@15Edo <eduard.salca@edu.unito.it>
2023-10-19 17:57:02 +0200

    ultimo commit

--- Esercizio3/trump.py
+++ Esercizio3/trump.py
@@ -5,20 +5,20 @@ from nltk import word_tokenize
 
 trump = pd.read_csv('Esercizio3\\trump_twitter_archive\\tweets.csv')
 sentences = list(trump['text'].apply(word_tokenize))
-tokens = [[token for token in sent if token.isalpha() ] for sent in sentences]
+tweets = [[token for token in sent if token.isalpha() ] for sent in sentences]
 
 
-train, vocab = padded_everygram_pipeline(2, sentences)
+padded_train, padded_vocab = padded_everygram_pipeline(2, tweets)
 tweet_like_a_Trump = MLE(2)
-tweet_like_a_Trump.fit(train, vocab)
+tweet_like_a_Trump.fit(padded_train, padded_vocab)
 
 print(tweet_like_a_Trump.generate(10))
 
 
 
-train, vocab = padded_everygram_pipeline(3, sentences)
+padded_train, padded_vocab = padded_everygram_pipeline(3, tweets)
 tweet_like_a_Trump = MLE(3)
-tweet_like_a_Trump.fit(train, vocab)
+tweet_like_a_Trump.fit(padded_train, padded_vocab)
 
 print(tweet_like_a_Trump.generate(9))
 
